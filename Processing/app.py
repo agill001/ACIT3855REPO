f01968396328d95ebedff8fc808e46ae83bb081d
agill001 <115107130+agill001@users.noreply.github.com>
2024-03-28 16:08:30 -0700

    showing flask cors agin in app

--- Processing/app.py
+++ Processing/app.py
@@ -152,7 +152,7 @@ app = connexion.FlaskApp(__name__, specification_dir='')
 CORS(app.app)
 app.app.config['CORS_HEADERS'] = 'Content-Type'
 
-app.add_api("openapi.yaml",
+app.add_api("openapi.yml",
             strict_validation=True,
             validate_responses=True)
 
