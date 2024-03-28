2a59b97a27e0a19ecf215117f6005e06a404d10c
agill001 <115107130+agill001@users.noreply.github.com>
2024-03-27 22:09:16 -0700

    added dashboard ui folder

---
+++ dashboard-ui/src/components/AppStats.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+export default function AppStats() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [stats, setStats] = useState({});
+    const [error, setError] = useState(null)
+
+    const getStats = () => {
+        fetch(`http://acit3855lab6.eastus.cloudapp.azure.com:8100/stats`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log("Received Stats")
+                    setStats(result);
+                    setIsLoaded(true);
+                },
+                (error) => {
+                    setError(error)
+                    setIsLoaded(true);
+                })
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+        return () => clearInterval(interval);
+    }, [getStats]);
+
+
+
+    if (error) {
+        return (<div className={"error"}>Error found when fetching from API</div>)
+    } else if (!isLoaded) {
+        return (<div>Loading...</div>)
+    } else {
+        return (
+            <div>
+                <h1>Latest Stats</h1>
+                <table className={"StatsTable"}>
+                    <tbody>
+                        <tr>
+                            <th>Create Posts</th>
+                            <th>Follow Events</th>
+                            <th>Total Likes</th>
+                            <th>Total Follow Count</th>
+                        </tr>
+                        <tr>
+                            <td># Posts: {stats['num_create_posts']}</td>
+                            <td># Events: {stats['num_follow_events']}</td>
+                            <td># Likes: {stats['total_likes']}</td>
+                            <td># Follows: {stats['total_follow_count']}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h3>Last Updated: {stats['last_updated']}</h3>
+            </div>
+        )
+    }
+}
